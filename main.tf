81ccf178b3ec4e0389dfc05749ad559c98d74758
leviankilevitch <126286194+leviankilevitch@users.noreply.github.com>
2023-07-02 19:03:20 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -34,6 +34,7 @@ module "blog_sg" {
 
   vpc_id  = data.aws_vpc.default.id
   name    = "blog"
+
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
