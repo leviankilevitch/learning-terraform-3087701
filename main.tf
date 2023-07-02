bc03199f7ef312f923414245e8dd128d75ae9f47
leviankilevitch <126286194+leviankilevitch@users.noreply.github.com>
2023-07-02 00:29:22 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
 
   enable_nat_gateway = true
 
@@ -34,7 +34,6 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  //subnet_id              = module.blog_vpc.public_subnets[0]
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
