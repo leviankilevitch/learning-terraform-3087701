f2b87e2c8dc90c0dcee58769d473734e17913ec6
leviankilevitch <126286194+leviankilevitch@users.noreply.github.com>
2023-07-02 18:30:26 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "blog" {
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
-  version = "4.13.0"
+  version = "5.1.0"
 
   vpc_id  = data.aws_vpc.default.id
   name    = "blog"
